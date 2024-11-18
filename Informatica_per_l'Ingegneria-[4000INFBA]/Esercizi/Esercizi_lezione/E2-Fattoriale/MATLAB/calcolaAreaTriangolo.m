65558b3c5f5a2ebc0b36f50c1bd5a5eb379de956
Michele La Riccia <88498231+IWUZMiKE@users.noreply.github.com>
2024-11-18 18:04:53 +0100

    Delete Informatica_per_l'Ingegneria-[4000INFBA]/Esercizi/Esercizi_lezione/E2-Fattoriale/MATLAB directory

--- Informatica_per_l'Ingegneria-[4000INFBA]/Esercizi/Esercizi_lezione/E2-Fattoriale/MATLAB/calcolaAreaTriangolo.m
+++
@@ -1,8 +0,0 @@
-function area = calcolaAreaTriangolo(base, altezza)
-%CALCOLAAREATRIANGOLO Calcola l'area di un triangolo generico.
-%   Questa funzione utilizza la formula geometrica del calcolo dell'area di
-%   un triangolo partendo dai valori della base e dell'altezza.
-
-area = base * altezza / 3;
-
-end
